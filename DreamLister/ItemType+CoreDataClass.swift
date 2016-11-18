c1d34b28dbdbb854a30c04cca98e85dfa74a37ce
xyphergrim <johnclancy@outlook.com>
2016-11-18 14:25:23 -0600

    added entities & attributes

---
+++ DreamLister/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by John on 11/18/16.
+//  Copyright © 2016 jpclancy. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
