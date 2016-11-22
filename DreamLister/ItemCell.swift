dec1c2093a567e59b894a584ef3122e6a6e2da7a
xyphergrim <johnclancy@outlook.com>
2016-11-22 12:41:04 -0600

    added iboutlets and tableview funds

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by John on 11/22/16.
+//  Copyright © 2016 jpclancy. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
